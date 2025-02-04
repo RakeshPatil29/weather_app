3dd2f4313bed690733058cca7b747a306cdc575d
Rakesh Patil <rp5175564@gmail.com>
2025-02-04 18:26:48 +0530

    update

---
+++ script.js
@@ -0,0 +1,63 @@
+const apiKey = "3f0f6af49b5cb2fe740e95d78672547a";
+const URL = "https://api.openweathermap.org/data/2.5/weather?units=metric&";
+
+const searchBar = document.querySelector("#search");
+const searchBtn = document.querySelector("#btn");
+
+const showTemp = document.querySelector(".show-temp");
+const showHumidity = document.querySelector(".show-humidity");
+const showSpeed = document.querySelector(".show-speed");
+
+const weatherIcon = document.querySelector("#icon");
+
+
+const getFacts = async (city) => {
+    let responce = await fetch(URL + `q=${city}` + `&appid=${apiKey}`);
+    console.log(responce);
+    let data = await responce.json();
+    console.log(data);
+    console.log(200, city);
+
+    showTemp.innerHTML = Math.round(data.main.temp) + " " +"ºC";
+
+    if (data.weather[0].main == "Clouds") {
+        weatherIcon.src = "../static/clouds.png";
+    } else if (data.weather[0].main == "Clear") {
+        weatherIcon.src = "../static/sun.png";
+    } else if (data.weather[0].main == "Rain") {
+        weatherIcon.src = "../static/rain.png"
+    } else if (data.weather[0].main == "Drizzle") {
+        weatherIcon.src = "../static/drizzle.png"
+    } else if (data.weather[0].main == "Mist") {
+        weatherIcon.src = "../static/mist.png";
+    } else if (data.weather[0].main == "Snow") {
+        weatherIcon.src = "../static/snow.png";
+    }
+
+    showTemp.style.fontSize = "x-large";
+    showTemp.style.fontWeight = "bolder";
+    showTemp.style.color = "white";
+
+    document.querySelector("#city").innerHTML = data.name;
+
+
+    showHumidity.innerHTML = data.main.humidity + "%";
+    showHumidity.style.fontSize = "x-large";
+    showHumidity.style.fontWeight = "bolder";
+    showHumidity.style.color = "white"
+
+    showSpeed.innerHTML = data.wind.speed + "km/h";
+    showSpeed.style.fontSize = "x-large";
+    showSpeed.style.fontWeight = "bolder";
+    showSpeed.style.color = "white"
+
+
+}
+
+searchBtn.addEventListener("click", () => {
+    // console.log(searchBar.value);
+    getFacts(searchBar.value);
+
+})
+
+getFacts();
